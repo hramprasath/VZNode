a27b40a6408cb1682397150031b51df43382f7c4
hramprasath <hramprasath@gmail.com>
2016-09-02 09:41:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-var port = process.env.PORT || 8085; // set our port
+var port = process.env.PORT || 5000; // set our port
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router(); // get an instance of the express Router
