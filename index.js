7b1a5fcaa55bb3631820033288a09ebd741ea549
hramprasath <hramprasath@gmail.com>
2016-09-04 12:17:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 
+var express = require('express'); // call express
+var app = express(); // define our app using express
+var bodyParser = require('body-parser');
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+console.log('my message server started...');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var port = process.env.PORT || 5000; // set our port
+// ROUTES FOR OUR API
+// =============================================================================
 var router = express.Router(); // get an instance of the express Router
 // test route to make sure everything is working (accessed at GET http://localhost:8085/api)
 router.get('/', function (req, res) {
@@ -13,10 +24,7 @@ router.post('/webhook', function (req, res) {
         speech: "Hi,there. I am Ent, an entertainment bot.  Would you like to see some recommendations for tonight?",
         displayText: "TV Recommendations",
         data: {     },
-        source: "Zero Service - app_zero.js"
-    }
-                
-                
+        source: "Zero Service - app_zero.js"    }
                 
             };
             break;
